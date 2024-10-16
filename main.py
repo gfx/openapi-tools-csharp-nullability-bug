3ced9d2aad96b79903780337d8a58ff41fdc63a8
FUJI Goro <g.psy.va@gmail.com>
2024-10-16 11:25:39 +0900

    add an endpoint that returns an object with a nullable field

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from openapi-tools-csharp-nullable!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class HelloWorldResponse(BaseModel):
+    message: str | None
+
+@app.get("/")
+def get_root() -> HelloWorldResponse:
+    return HelloWorldResponse(message="Hello, World!")
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8001)
